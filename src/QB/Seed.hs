9a0e32aee0313c8a5aa925a635fddf7aeaf41cff
ishiy <y.ishihara.1993@gmail.com>
2018-12-19 17:54:15 +0900

    Add yaml parser

--- src/QB/Seed.hs
+++ src/QB/Seed.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module QB.Seed where
+
+import Data.ByteString (ByteString)
+import Data.Yaml
 
 import QB.Types (FmrType)
 
@@ -9,4 +13,22 @@ data Seed = Seed
   , elemType :: FmrType
   , eomRank :: Int -- 時間1階微分を計算するのに必要な空間微分の階数の最大値
   , withFilter :: Bool
-  }
+  } deriving (Show,Eq)
+
+instance FromJSON Seed where
+  parseJSON (Object v) =
+    Seed <$> v .: "scheme"
+         <*> v .: "axes"
+         <*> v .: "bases"
+         <*> v .: "elem-type"
+         <*> v .: "eom-rank"
+         <*> v .: "with-filter"
+  parseJSON _ = fail "Expected Object for Seed value"
+
+getSeed :: FilePath -> IO Seed
+getSeed = decodeFileThrow
+
+decodeSeed :: ByteString -> Maybe Seed
+decodeSeed xs = case decodeEither' xs of
+                  Right s -> Just s
+                  Left _ -> Nothing
---
+++ test/SeedSpec.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+module SeedSpec (spec) where
+
+import Test.Hspec
+
+import QB.Seed
+
+spec :: Spec
+spec = do
+  describe "Unit test" $ do
+    let seed = Seed { scheme = "sl4th3"
+                    , axes = ["x","y"]
+                    , bases = ["r","u","v","p"]
+                    , elemType = "double"
+                    , eomRank = 2
+                    , withFilter = False
+                    }
+        -- x だけだと bool値 として解釈される
+        yaml = "scheme: sl4th3\naxes: ['x','y']\nbases: [r, u, v, p]\nelem-type: double\neom-rank: 2\nwith-filter: false\n"
+    it "check decoding yaml" $
+      decodeSeed yaml `shouldBe` (Just seed)
