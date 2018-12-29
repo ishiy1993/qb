a68b7c764f941debb5d77f2887b83109ca80b0c6
ishiy <y.ishihara.1993@gmail.com>
2018-12-29 19:30:48 +0900

    Add Format

--- src/QB/Exp.hs
+++ src/QB/Exp.hs
@@ -1,7 +1,13 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module QB.Exp where
 
+import qualified Data.ByteString.Builder as B
 import qualified Data.Map.Strict as M
+import System.IO
+
+import QB.Pretty
 
 type Ident = String
 
@@ -36,3 +42,38 @@ data Coord = I0 | I1 | I2 | I3
 
 build :: [Coord] -> Coords
 build = foldr (\c acc -> M.insertWith (+) c 1 acc) M.empty
+
+class Format a where
+  formatWith :: [String] -> a -> B.Builder
+
+instance Format Equations where
+  formatWith as = sepWith "\n" . map (formatWith as)
+
+instance Format Equation where
+  formatWith as (Equation {lhs = l, rhs = r}) = formatWith as l <=> formatWith as r
+
+instance Format Exp where
+  formatWith _ (Imm x) = B.doubleDec x
+  formatWith as (Sym n _ ds) = B.stringUtf8 n <> formatWith as ds
+  formatWith as (Neg e) = "-" <> formatWith as e
+  formatWith as (Add e1 e2) = formatOp as "+" e1 e2
+  formatWith as (Sub e1 e2) = formatOp as "-" e1 e2
+  formatWith as (Mul e1 e2) = formatOp as "*" e1 e2
+  formatWith as (Div e1 e2) = formatOp as "/" e1 e2
+  formatWith as (Pow e1 e2) = formatOp as "**" e1 e2
+
+instance Format Coords where
+  formatWith as ds | M.null ds = ""
+                   | otherwise = "_" <> mconcat [mconcat $ replicate i (formatWith as d) | (d,i) <- M.toAscList ds]
+
+instance Format Coord where
+  formatWith as I0 = B.stringUtf8 $ as !! 0
+  formatWith as I1 = B.stringUtf8 $ as !! 1
+  formatWith as I2 = B.stringUtf8 $ as !! 2
+  formatWith as I3 = B.stringUtf8 $ as !! 3
+
+printWith :: Format a => [String] -> a -> IO ()
+printWith as = B.hPutBuilder stdout . formatWith as
+
+formatOp :: Format a => [String] -> B.Builder -> a -> a -> B.Builder
+formatOp as op e1 e2 = "(" <> formatWith as e1 <> " " <> op <> " " <> formatWith as e2 <> ")"
